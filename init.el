e3dec94379b48c9e4eeb9e14aa35000198aa9ad1
Xiao Hanyu <xiaohanyu1988@gmail.com>
2013-08-30 15:54:59 +0800

    Better support for newer version org-modes init.
    You can download the latest org-mode source code, then use
    "ORG_HOME=/path/to/org-mode/ emacs" to boot your emacs, then you can try the
    latest org-mode within emacs. Maybe you need something like "(require 'ox)" or
    "(require 'ox-md)" to make org-mode works, which depends on your interest.

--- init.el
+++ init.el
@@ -6,10 +6,14 @@
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
-(when (getenv "ORG_HOME")
-  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
+(setq org-home (getenv "ORG_HOME"))
+
+(when org-home
+  (let ((org-lisp-dir (expand-file-name "lisp" org-home))
+        (org-contrib-lisp-dir (expand-file-name "contrib/lisp" org-home)))
     (when (file-directory-p org-lisp-dir)
       (add-to-list 'load-path org-lisp-dir)
+      (add-to-list 'load-path org-contrib-lisp-dir)
       (require 'org))))
 
 ;; load the ome from the `after-init-hook' so all packages are loaded
@@ -19,9 +23,8 @@
     (setq ome-dir
           ,(file-name-directory (or load-file-name (buffer-file-name))))
     ;; only load org-mode later if we didn't load it just now
-    ,(unless (and (getenv "ORG_HOME")
-                  (file-directory-p (expand-file-name "lisp"
-                                                      (getenv "ORG_HOME"))))
+    ,(unless (and org-home
+                  (file-directory-p (expand-file-name "lisp" org-home)))
        '(require 'org))
     ;; load up the ome
     (org-babel-load-file (expand-file-name "ome.org" ome-dir))))
