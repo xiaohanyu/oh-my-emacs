1826bc723964b57e34ae05e77182d4b16958f199
Xiao Hanyu <xiaohanyu1988@gmail.com>
2013-09-13 23:07:00 +0800

    Add some code snippets to custom.el

---
+++ custom.el
@@ -0,0 +1,64 @@
+;;; This file contains some temporary code snippets, it will be loaded after
+;;; various oh-my-emacs modules. When you just want to test some code snippets
+;;; and don't want to bother with the huge ome.*org files, you can put things
+;;; here.
+
+;; For example, oh-my-emacs disables menu-bar-mode by default. If you want it
+;; back, just put following code here.
+(menu-bar-mode t)
+
+;;; You email address
+(setq user-mail-address "xiaohanyu1988@gmail.com")
+
+;;; Calendar settings
+;; you can use M-x sunrise-sunset to get the sun time
+(setq calendar-latitude 39.9)
+(setq calendar-longitude 116.3)
+(setq calendar-location-name "Beijing, China")
+
+;;; Time related settings
+;; show time in 24hours format
+(setq display-time-24hr-format t)
+;; show time and date
+(setq display-time-and-date t)
+;; time change interval
+(setq display-time-interval 10)
+;; show time
+(display-time-mode t)
+
+;;; Some tiny tool functions
+(defun replace-all-chinese-quote ()
+  (interactive)
+  (save-excursion
+    (mark-whole-buffer)
+    (replace-regexp  "”\\|“" "\"")
+    (mark-whole-buffer)
+    (replace-regexp "’\\|‘" "'")))
+
+;; Comment function for GAS assembly language
+(defun gas-comment-region (start end)
+  "Comment region for AT&T syntax assembly language The default
+comment-char for gas is ';', we need '#' instead"
+  (interactive "r")
+  (setq end (copy-marker end t))
+  (save-excursion
+    (goto-char start)
+    (while (< (point) end)
+      (beginning-of-line)
+      (insert "# ")
+      (next-line))
+    (goto-char end)))
+
+(defun gas-uncomment-region (start end)
+  "Uncomment region for AT&T syntax assembly language the
+inversion of gas-comment-region"
+  (interactive "r")
+  (setq end (copy-marker end t))
+  (save-excursion
+    (goto-char start)
+    (while (< (point) end)
+      (beginning-of-line)
+      (if (equal (char-after) ?#)
+          (delete-char 1))
+      (next-line))
+    (goto-char end)))
